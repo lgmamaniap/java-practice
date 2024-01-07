4237f91a4fc47c9a44a8472cbc608b91766979f8
gustavo <lgmamaniap@gmail.com>
2024-01-07 00:18:02 -0400

    feat: agregando nueva clase de practicas

---
+++ src/main/java/org/example/clases/Alumno.java
@@ -0,0 +1,20 @@
+package org.example.clases;
+
+public class Alumno {
+    int id;
+    String nombre;
+    String apellido;
+
+    public void mostrarNombre() {
+        System.out.println("Hola, soy un alumno y se decir mi nombre");
+    }
+
+    public void saberAprobado(double calificacion) {
+        if (calificacion > 50) {
+            System.out.println("Aprobado");
+            return;
+        }
+
+        System.out.println("Reprobado");
+    }
+}
