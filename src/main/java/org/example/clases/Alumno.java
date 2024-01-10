357f494965c0206121fa0913ef01ee1e528cd96c
gustavo <lgmamaniap@gmail.com>
2024-01-09 23:44:18 -0400

    feat: agregando constructor en clase alumnos

--- src/main/java/org/example/clases/Alumno.java
+++ src/main/java/org/example/clases/Alumno.java
@@ -5,6 +5,14 @@ public class Alumno {
     String nombre;
     String apellido;
 
+    public Alumno() {}
+
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
     public void mostrarNombre() {
         System.out.println("Hola, soy un alumno y se decir mi nombre");
     }
