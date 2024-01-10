992f381e450d7012c0a98aefe32e13bfa9ebc455
gustavo <lgmamaniap@gmail.com>
2024-01-10 17:24:15 -0400

    feat: agregando getters y setters en clase alumno

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,29 @@ public class Main {
         Alumno alumno1 = new Alumno();
         Alumno alumno2 = new Alumno(1, "Juan", "Perez");
 
+        System.out.println("La id del alumno 2 es: " + alumno2.getId());
+        System.out.println("El nombre del alumno 2 es: " + alumno2.getNombre());
+        System.out.println("El apellido del alumno 2 es: " + alumno2.getApellido());
+
+        // Asignando valores a alumno1
+        alumno1.setId(2);
+        alumno1.setNombre("Maria");
+        alumno1.setApellido("Lopez");
+
+        System.out.println("--------------------");
+        System.out.println("La id del alumno 1 es: " + alumno1.getId());
+        System.out.println("El nombre del alumno 1 es: " + alumno1.getNombre());
+        System.out.println("El apellido del alumno 1 es: " + alumno1.getApellido());
+
+        // Modificando valores de alumno1
+        alumno1.setId(3);
+        alumno1.setNombre("Pedro");
+        alumno1.setApellido("Gomez");
+
+        System.out.println("--------------------");
+        System.out.println("La id del alumno 1 es: " + alumno1.getId());
+        System.out.println("El nombre del alumno 1 es: " + alumno1.getNombre());
+        System.out.println("El apellido del alumno 1 es: " + alumno1.getApellido());
+        
     }
 }
--- src/main/java/org/example/clases/Alumno.java
+++ src/main/java/org/example/clases/Alumno.java
@@ -13,6 +13,32 @@ public class Alumno {
         this.apellido = apellido;
     }
 
+    // Getters y setters
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Metodos
     public void mostrarNombre() {
         System.out.println("Hola, soy un alumno y se decir mi nombre");
     }
