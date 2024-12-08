039d9e094e2d814e6945f531925d53d48f0d1b58
gustavo <lgmamaniap@gmail.com>
2024-12-08 17:08:56 -0400

    feat: for loop

--- EstructurasRepetitivas/src/estructurasrepetitivas/EstructurasRepetitivas.java
+++ EstructurasRepetitivas/src/estructurasrepetitivas/EstructurasRepetitivas.java
@@ -3,13 +3,19 @@ package estructurasrepetitivas;
 public class EstructurasRepetitivas {
 
     public static void main(String[] args) {
-        // Bucle do while
+        // do while loop
         int counter = 0;
         do{
             System.out.println(counter);
             counter++;
         }
         while(counter < 10);
+        
+        // For loop
+        int forCounter = 10;
+        for (int i = 0; i < forCounter; i++) {
+            System.out.println(i);
+        }
     }
     
 }
